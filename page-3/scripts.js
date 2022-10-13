ce59778b95b3824f286373747a768586695f2470
Azat482 <4est326@gmail.com>
2022-10-14 01:51:21 +0300

    update page 3, adaptive header 2

---
+++ page-3/scripts.js
@@ -0,0 +1,13 @@
+(function() {
+    "use strict";
+    var toggle = document.querySelector(".header__top-hamburger-btn");
+    toggleHandler(toggle);
+    function toggleHandler(toggle) {
+      toggle.addEventListener( "click", function(e) {
+        console.log("header button clicked");
+        e.preventDefault();
+        (this.classList.contains("header__top-hambtn-active") === true) ? this.classList.remove("header__top-hambtn-active") : this.classList.add("header__top-hambtn-active");
+        document.querySelector(".header__top-nav").classList.toggle("header__top-hamburger-list-active");
+    });
+    }
+  })();
