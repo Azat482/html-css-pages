3f03870ee3d2be2300e8ad3df65e018deffe79d1
Azat482 <4est326@gmail.com>
2022-12-14 02:26:53 +0300

    add adaptive design to page-2

--- page-2/gr6-slider.js
+++ page-2/gr6-slider.js
@@ -4,7 +4,7 @@ const slider_track = document.querySelector('.group-6-items-box');
 const slider_item = document.querySelector('.gr6-slider-item');
 const slider_items_count = document.querySelectorAll('.gr6-slider-item').length;
 
-const show_slider_item = 3;
+let show_slider_item = 3;
 const slider_item_border = 4;
 const slider_item_width = slider_item.clientWidth + slider_item_border * 2;
 const slider_item_gap = 20;
@@ -12,9 +12,13 @@ const slider_item_shift = slider_item_width + slider_item_gap;
 let position = 0;
 let position_index = 0;
 
+window.addEventListener('resize', function(e){
+    set_slides_num();
+}, true)
+
 prev_button.addEventListener('click', ()=>{
     console.log('previous slide', slider_items_count);
-    if(position_index){
+    if(position_index) {
         position += slider_item_shift;
         position_index--;
     }
@@ -34,3 +38,14 @@ next_button.addEventListener('click', ()=>{
 const set_position = (position) => {
     slider_track.style.transform = `translateX(${position}px)`;
 };
+
+const set_slides_num = ()=>{
+    if(this.window.innerWidth >= 992 && this.window.innerWidth < 1400){
+        show_slider_item = 2;
+    }
+    if(this.window.innerWidth < 992){
+        show_slider_item = 1;
+    }
+}
+
+set_slides_num();
